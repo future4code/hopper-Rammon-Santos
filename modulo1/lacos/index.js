79d9f95334ecf262bfeffe8a56b0f92f76c4e4c0
Rammon <rrs@Rammon.local>
2022-03-08 21:41:25 -0300

    exercicio incompleto lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,53 @@
+// let valor = 0
+// for(let i = 0; i < 5; i++) {
+//   valor += i
+// }
+// console.log(valor)//10
+
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// for (let numero of lista) {
+//   if (numero > 18) {
+// 		console.log(numero)
+// 	}
+// }
+
+// const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+// let quantidadeAtual = 0
+// while(quantidadeAtual < quantidadeTotal){
+//   let linha = ""
+//   for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+//     linha += 1
+//   }
+//   console.log(linha)
+//   quantidadeAtual++
+// }
+
+//EXERCICIO 
+
+
+
+
+
+let quantidadeAnimais = Number(prompt("Digite quantos animais voce tem"));
+
+// const nomeDosAnimais = [""];
+
+while (quantidadeAnimais < 0) {
+    
+    
+        console.log(quantidadeAnimais, "Que pena! Você pode adotar um pet!");
+        
+       
+}
+
+
+
+   
+
+
+
+
+
+
+
+
