f778cdbe2951f7a4ca3c700bdab4c4178d9acaeb
Rammon <rrs@Rammon.local>
2022-03-11 20:48:27 -0300

    Finalizado projeto com novas alteracoeo de funcionamento

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -1,26 +1,7 @@
- //armazenando 
-let usuarioCarta1 = [] 
-let usuarioCarta2 = []
-let computadorCarta1 = []
-let computadorCart2 = []
+//  //armazenando 
+// let usuarioCarta1 = [] 
+// let computadorCarta1 = []
 
- 
- //codigo para iniciar o jogo             pular linha
- if(confirm("Bem vindo ao jogo BlackJack" + "\n" + "Quer iniciar uma nova rodada?")) {
-   //SORTEAR 2 CARTAS
-   usuarioCarta1.push(comprarCarta())
-   usuarioCarta2.push(comprarCarta())
-   computadorCarta1.push(comprarCarta())
-   computadorCart2.push(comprarCarta())
-   
-   let cartasUsuario = [usuarioCarta1, usuarioCarta2]
-   let cartasComputador = [computadorCarta1]
-
-   console.log(cartasUsuario, cartasComputador)
-
- } else {
-    alert("o jogo acabou")
- } 
 
 // TENTAR USAR ESTRUTURA SWITE CASE =================================================
 // if (usuario, computador){
@@ -30,23 +11,8 @@ let computadorCart2 = []
 
 
 
-
-
 // //CALCULO DOS RESULTADOS DAS CARTAS   
-// const CalculoUsuario = usuario + usuario; //somar o valor da pontuacao
-// const CalculoComputador = computador + computador; // ˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆ
+// const CalculoUsuario = usuario + usuario //somar o valor da pontuacao
+// const CalculoComputador = computador + computador // ˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆˆ
 
 
-/*
-  //CODIGO EM FUNCAO PARA O RESULTADO DO JOGO
-  const resultado = () => {
-   if (CalculoUsuario === CalculoComputador) {
-          console.log("Empate!")
-    } else if (CalculoUsuario > CalculoComputador) {
-          console.log("O usuário ganhou!")
-    } else if (CalculoUsuario < CalculoComputador) {
-             console.log("O computador ganhou!")
-    }
-}
- resultado()
- */
